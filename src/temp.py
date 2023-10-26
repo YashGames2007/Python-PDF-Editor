a0091fe68c9b36309984b3094b7295c516516267
YashGames2007 <yashbhavsar.dev2007@gmail.com>
2023-10-27 00:13:14 +0530

    PDF Editor: V1.7
    Added PDF Splitting Function

--- src/temp.py
+++ src/temp.py
@@ -1,15 +1,40 @@
+import fitz  # this is pymupdf
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import simpledialog
 
-def save_as():
+def print_selected_pages(input_pdf, output_pdf, pages_string):
+    doc = fitz.open(input_pdf)
+    new_doc = fitz.open()
+    pages = extract_pages(pages_string)
+    for i in pages:
+        new_doc.insert_pdf(doc, from_page=i, to_page=i)
+
+    new_doc.save(output_pdf)
+
+def extract_pages(input_string):
+    pages = []
+    for part in input_string.split(','):
+        if '-' in part:
+            start, end = map(int, part.split('-'))
+            pages.extend(range(start, end+1))
+        else:
+            pages.append(int(part))
+    return [page-1 for page in pages]
+
+def get_pages():
+    # Create a simple dialog to get user input
     root = tk.Tk()
-    root.withdraw()  # to hide the main window
+    root.withdraw()  # Hide the main window
+    pages_str = simpledialog.askstring("Page Numbers", "Enter the page numbers (comma-separated or range):")
+    return pages_str
 
-    # open save file dialog
-    file_path = filedialog.asksaveasfilename(defaultextension=".pdf", initialfile='Merged.pdf', filetypes=(("PDF files", "*.pdf"), ("All files", "*.*")))
+# Call the function with the input PDF file and the output file
+pages_to_print = get_pages()
+# print(pages_to_print)
 
-    if file_path:
-        print(f"File will be saved at: {file_path}")
-        # Here you can add the code to write the PDF file to `file_path`
 
-save_as()
+
+# Test the function
+# print(extract_pages("1,3,5-7, 13, 2-4"))
+
+print_selected_pages('D:\\Programming files\\GitHub\\Repositories\\Python-PDF-Editor\\MML Formulae-1.pdf', 'output.pdf', pages_to_print)
