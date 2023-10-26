b01d41007dac54e571ba799a0d6497ffd71f2594
YashGames2007 <yashbhavsar.dev2007@gmail.com>
2023-10-26 22:21:42 +0530

    PDF Editor: V1.6
    Added PDF Merging Function.

--- src/temp.py
+++ src/temp.py
@@ -1,30 +1,15 @@
 import tkinter as tk
+from tkinter import filedialog
 
-class YourClass:
-    def __init__(self, master):
-        self.master = master
-        self.password = ""
-        self.password_window = None
-        self.password_entry = None
+def save_as():
+    root = tk.Tk()
+    root.withdraw()  # to hide the main window
 
-    def open_pdf(self):
-        password = self.password_entry.get()
-        self.password_window.destroy()
-        # pdf_file = fitz.open('your_pdf_file.pdf')
-        # if pdf_file.is_encrypted:
-        #     pdf_file.authenticate(password)
-        #     # ttk.messagebox.showinfo("Success", "PDF file opened successfully!")
-        self.password = password
+    # open save file dialog
+    file_path = filedialog.asksaveasfilename(defaultextension=".pdf", initialfile='Merged.pdf', filetypes=(("PDF files", "*.pdf"), ("All files", "*.*")))
 
-    def get_password(self):
-        self.password_window = tk.Toplevel(self.master)
-        self.password_window.title('Enter Password')
+    if file_path:
+        print(f"File will be saved at: {file_path}")
+        # Here you can add the code to write the PDF file to `file_path`
 
-        password_label = tk.Label(self.password_window, text="Enter Password:")
-        password_label.pack()
-
-        self.password_entry = tk.Entry(self.password_window, show="*")
-        self.password_entry.pack()
-
-        submit_button = tk.Button(self.password_window, text="Submit", command=self.open_pdf)
-        submit_button.pack()
+save_as()
